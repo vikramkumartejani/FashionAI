266ec0d9b6453f8056123e3164e6f5e23b8dd75c
vikramkumartejani <vikramkumar8t8@gmail.com>
2023-11-03 18:47:45 +0000

    'update'

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,31 +1,47 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './products.css'
 import Navbar from '../Navbar/Navbar'
 import Shoes from './Shoes'
 import Shirt from './Shirt'
 import Pants from './Pants'
+import LoadingSpinner from '../LoadingSpinner.js'
 const Products = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 500);
+  }, []);
   return (
     <>
-      <Navbar />
-      <div className='product-main-container'>
-        <div className='product-container'>
-          <div>
-            <h3>Shirts:</h3>
-            <Shirt />
-          </div>
-          <div>
-            <h3>Pants</h3>
-            <Pants />
-          </div>
-          <div>
-            <h3>Shoes:</h3>
-            <Shoes />
-          </div>
-        </div>
+      <div>
+        {isLoading ? (
+          <LoadingSpinner />
+        ) : (
+        <div>
+              <Navbar /><div className='product-main-container'>
+                <div className='product-container'>
+                  <div>
+                    <h3>Shirts:</h3>
+                    <Shirt />
+                  </div>
+                  <div>
+                    <h3>Pants</h3>
+                    <Pants />
+                  </div>
+                  <div>
+                    <h3>Shoes:</h3>
+                    <Shoes />
+                  </div>
+                </div>
+              </div></div>
+        )}
       </div>
     </>
   )
 }
 
 export default Products
+
+
